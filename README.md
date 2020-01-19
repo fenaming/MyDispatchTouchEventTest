5d1fb55e065b474e97ebe32dc843257ab04576e6
fzm <fengzhongming@yangufang.com>
2020-01-19 17:47:55 +0800

    测试ViewGroup事件分发、拦截与消费

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ this is a project to test dispatchtouchevent.
 
 # 结论
 
-1、触摸事件的传递流程是从dispatchTouchEvent开始的，如果不进行人为干预（默认返回父类同名函数），则事件将会依照嵌套层次由外层向内层传递，到达最内层的view时，就由它的onTouchEvent方法处理，该方法能够消费该事件则返回true，处理不了则返回false，并且事件开始重新向外层传递，由外层的onTouchEvent方法处理。。
+1、触摸事件的传递流程是从dispatchTouchEvent开始的，如果不进行人为干预（默认返回父类同名函数），则事件将会依照嵌套层次由外层向内层传递，到达最内层的view时，就由它的onTouchEvent方法处理，该方法能够消费该事件则返回true，处理不了则返回false，并且事件开始重新向外层传递，由外层的onTouchEvent方法处理。
 
 2、如果事件向内层传递时被人为干预，事件处理函数返回true，会导致事件被提前消费掉，内层的view不会收到这个事件。
 
 3、view控件的触发顺序是先执行onTouch方法，最后执行onClick方法，如果onTouch方法返回true，则事件不会被继续传递，最后不会执行到onClick方法。
+
+4、触摸事件的传递顺序是由Activity到ViewGroup，再由ViewGroup递归传递给它的子View。
+
+5、ViewGroup通过方法onInterceptTouchEvent对事件进行拦截，返回true时不会继续传递事件。
+
+6、在子View对事件消费之后，ViewGroup接收不到任何事件。
---
+++ app/src/main/java/com/fenaming/mydispatchtoucheventtest/baseview/MyRelativeLayout.java
@@ -0,0 +1,89 @@
+package com.fenaming.mydispatchtoucheventtest.baseview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
+
+/**
+ * @author fenaming
+ * @date 2020/1/19
+ * @e-mail fenaming@163.com
+ * @description RelativeLayout继承自ViewGroup
+ */
+public class MyRelativeLayout extends RelativeLayout {
+
+    private static final String TAG = "FZM-MyRelativeLayout";
+
+    public MyRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public MyRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log.i(TAG, "dispatchTouchEvent: ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG, "dispatchTouchEvent: ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.i(TAG, "dispatchTouchEvent: ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.i(TAG, "dispatchTouchEvent: ACTION_CANCEL");
+                break;
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log.i(TAG, "onInterceptTouchEvent: ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG, "onInterceptTouchEvent: ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.i(TAG, "onInterceptTouchEvent: ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.i(TAG, "onInterceptTouchEvent: ACTION_CANCEL");
+                break;
+            default:
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log.i(TAG, "onTouchEvent: ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG, "onTouchEvent: ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.i(TAG, "onTouchEvent: ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.i(TAG, "onTouchEvent: ACTION_CANCEL");
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+}
